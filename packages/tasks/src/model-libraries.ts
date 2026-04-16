4cc5c1bc59f7be6193807fe588f1719140247886
hyxie <hongyu.xie@horizon.auto>
2026-04-16 21:41:22 +0800

    Add HorizonLabs HoloBrain download counting rule

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -1106,6 +1106,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		repoUrl: "https://github.com/airockchip/rknn-llm",
 		countDownloads: `path_extension:"rkllm"`,
 	},
+	"horizonlabs-holobrain": {
+		prettyLabel: "HorizonLabs HoloBrain",
+		repoName: "HoloBrain",
+		repoUrl: "https://github.com/HorizonRobotics/RoboOrchardLab/tree/master/projects/holobrain",
+		docsUrl: "https://horizonrobotics.github.io/robot_lab/holobrain/",
+		filter: false,
+		countDownloads: `(path_extension:"safetensors" AND path_prefix:"pretrain/") OR (path_extension:"safetensors" AND path_prefix:"post_training_")`,
+	},
 	saelens: {
 		prettyLabel: "SAELens",
 		repoName: "SAELens",
